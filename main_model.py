30aa028de1885ce9006aedc115a5a3a793224cc7
Gustavo Leão <gustavodleao.dev@gmail.com>
2026-05-29 17:30:01 -0300

    Improve city data collection and API request flow
    Refactor city input and data storage logic, and update API connection handling.

--- main_model.py
+++ main_model.py
@@ -6,40 +6,52 @@
 import requests
 import os
 
+#FUNÇÃO DE LIMPEZA DO TERMINAL
 def clear():
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
 
-cidade_lista = []
-coord = []
+clear()
 
-cidade = input("Digite o nome da cidade: ")
-cidade_lista.append(cidade)
-
-latitude = input("Digite a latitude: ")
-coord.append(latitude)
-
-longitude = input("Digite a longitude: ")
-coord.append(longitude)
-
-url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true"
-
+#CRIAÇÃO DE ARQUIVO PARA ARMAZENAR DADOS
 while True:
-    try:
-        response = requests.get(url)
-        dados = response.json()
-        temperatura = dados["current_weather"]["temperature"]
-        clear()
-        print(f"A temperatura atual em {cidade} é: {temperatura}°C")
-        loop = input("Gostaria de continuar o programa? S/N\n").upper()
-        if loop == "S":
-            clear()
-            continue
-        else:
-            break
-    except Exception:
-        clear()
-        print("Dados não encontrados.")
+    cidade = input("Digite o nome da cidade: ")
+    if os.path.exists("Cidades") == False:
+        os.mkdir("Cidades")
+
+        path = os.path.join("Cidades", f"{cidade}.py")
+
+        latitude = input("Digite a latitude: ")
+        longitude = input("Digite a longitude: ")
+        with open(path, "w") as f:
+            f.write(f'coord1 = ["{latitude}"]\n')
+            f.write(f'coord2 = ["{longitude}"]\n')
         break
+    else:
+        break
+        
+''' ERRO NO URL, DIZ QUE NÃO FOI DEFINIDO.
+    ENTENDER POR QUE NÃO PASSA PELA VERIFICAÇÃO
+    DO WHILE ACIMA '''
+
+#CONEXÃO DA API COM O SITE
+url = f"https://api.open-meteo.com/v1/forecast?latitude={coord1[0]}&longitude={coord2[0]}&current_weather=true"
+try:
+    response = requests.get(url)
+    dados = response.json()
+    temperatura = dados["current_weather"]["temperature"]
+    clear()
+    print(f"A temperatura atual em {cidade} é: {temperatura}°C")
+    loop = input("Gostaria de continuar o programa? S/N\n").upper()
+    #LOOP PRA CONSERTAR, DEIXA O PROGRAMA LIGADO
+    if loop == "S":
+        clear()
+        continue
+    else:
+        clear()
+        break
+except Exception:
+    print("Dados não encontrados.")
+    break
