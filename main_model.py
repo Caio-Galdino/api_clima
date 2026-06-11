7d52f88fde89e5ae16c3fdf10bb4e507a7b89e4e
Gustavo Leão <gustavodleao.dev@gmail.com>
2026-06-11 14:21:03 -0300

    Refactor city data handling and API integration

--- main_model.py
+++ main_model.py
@@ -1,12 +1,12 @@
-''' AINDA FALTA ENCONTRAR UM JEITO DE CATALOGAR -CIDADE,LATITUDE,LONGITUDE-
-    CRIAR ARQUIVO CUJO NOME SEJA O DA CIDADE, E DENTRO DELE PÔR UMA LISTA
-    COM AS DUAS COORDENADAS DA CIDADE. DEPOIS AUTOMATIZAR O PROCESSO
-    DE CRIAÇÃO DO ARQUIVO + LISTA, E DE ESCOLHA DA CIDADE DENTRO DO WHILE'''
-
 import requests
 import os
+import json
 
-#FUNÇÃO DE LIMPEZA DO TERMINAL
+def options():
+    print("1. Iniciar programa\n"
+          "2. Cadastrar cidade nova\n")
+    
+
 def clear():
     if os.name == "nt":
         os.system("cls")
@@ -15,43 +15,72 @@ def clear():
 
 clear()
 
-#CRIAÇÃO DE ARQUIVO PARA ARMAZENAR DADOS
+try:
+    with open("dados.json", "r") as pull_city:
+        dados = json.load(pull_city)
+except:
+    print("Erro.")
+
+#Opções de ação (registrar novas coordenadas)
 while True:
-    cidade = input("Digite o nome da cidade: ")
-    if os.path.exists("Cidades") == False:
-        os.mkdir("Cidades")
-
-        path = os.path.join("Cidades", f"{cidade}.py")
-
+    options()
+    escolha = int(input("Digite a opção desejada: "))
+    #"Rodar o programa"
+    if escolha == 1:
+        clear()
+        break
+    #"Registrar nova cidade"
+    elif escolha == 2:
+        cidade = input("Digite o nome da cidade (sem acentuação): ").upper()
         latitude = input("Digite a latitude: ")
         longitude = input("Digite a longitude: ")
-        with open(path, "w") as f:
-            f.write(f'coord1 = ["{latitude}"]\n')
-            f.write(f'coord2 = ["{longitude}"]\n')
-        break
-    else:
-        break
-        
-''' ERRO NO URL, DIZ QUE NÃO FOI DEFINIDO.
-    ENTENDER POR QUE NÃO PASSA PELA VERIFICAÇÃO
-    DO WHILE ACIMA '''
+        #Criação em arquivo JSON
+        nova_cidade = {
+            "cidade": cidade,
+            "latitude": latitude,
+            "longitude": longitude
+            }
+        try:
+            with open("dados.json", "r") as arquivo:
+                dados = json.load(arquivo)
+        except:
+            dados = []
 
-#CONEXÃO DA API COM O SITE
-url = f"https://api.open-meteo.com/v1/forecast?latitude={coord1[0]}&longitude={coord2[0]}&current_weather=true"
-try:
-    response = requests.get(url)
-    dados = response.json()
-    temperatura = dados["current_weather"]["temperature"]
-    clear()
-    print(f"A temperatura atual em {cidade} é: {temperatura}°C")
-    loop = input("Gostaria de continuar o programa? S/N\n").upper()
-    #LOOP PRA CONSERTAR, DEIXA O PROGRAMA LIGADO
-    if loop == "S":
+        dados.append(nova_cidade)
+        with open("dados.json", "w") as arquivo:
+            json.dump(dados,arquivo,indent=4)
         clear()
-        continue
     else:
-        clear()
+        print("Inválido.")
+        continue
+
+#Busca as coordenadas das cidades no arquivo JSON 
+cidade_encontrada = None
+for cidade in dados:
+    if cidade["cidade"] == pull_city:
+        cidade_encontrada = cidade
+        latitude = cidade_encontrada["latitude"]
+        longitude = cidade_encontrada["longitude"]
         break
-except Exception:
-    print("Dados não encontrados.")
-    break
+    else:
+        cidade_encontrada = cidade
+        latitude = cidade_encontrada["latitude"]
+        longitude = cidade_encontrada["longitude"]
+    #Conexão com a API
+    url = (f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true")
+
+    response = requests.get(url)
+    dados_api = response.json()
+
+    temperatura = dados_api["current_weather"]["temperature"]
+    clima = dados_api["current_weather"]["is_day"]
+    if clima == 1:
+        clima = "dia"
+    else:
+        clima = "noite"
+    #Output de informações para o usuário
+    print(
+        f"A temperatura atual em {cidade_encontrada["cidade"]} é {temperatura}°C\n"
+        f"No momento, {cidade_encontrada["cidade"]} está de {clima}.\n"
+        )
+    #Falta mostrar o clima atual (se ta chovendo ou fazendo sol)
